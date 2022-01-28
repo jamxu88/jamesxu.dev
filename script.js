8dc5582bcabf144785948ba917edc62b543fd9b3
James <james@jamesxu.dev>
2022-01-28 08:26:19 -0500

    revision

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+particlesJS.load('particles-js', 'config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
